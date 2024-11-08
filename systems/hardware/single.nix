ab2f682218c62dc999a42ed1bc5cd73343173b25
Elias <iychash@gmail.com>
2024-11-07 21:11:05 -0700

    force GiB

--- systems/hardware/single.nix
+++ systems/hardware/single.nix
@@ -51,7 +51,7 @@
 	        mount /dev/mapper/luksbtrfs /mnt
 	        btrfs quota enable /mnt
                 btrfs qgroup limit 64G /mnt/nix
-		totalFilesystemSize=$(btrfs filesystem usage /mnt | grep "Device size:" | \
+		totalFilesystemSize=$(btrfs filesystem usage -g /mnt | grep "Device size:" | \
 		    sed -n 's/Device size:[[:space:]]*\([0-9.]*\)GiB/\1/p')
 		homeQuotaSize=$(awk "BEGIN {print int($totalFilesystemSize - 72)}")
 	      	btrfs qgroup limit ''${homeQuotaSize}G /mnt/home
