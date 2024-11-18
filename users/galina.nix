e2d8546713e42ccb1e063146f26e64d6b77ab245
elias-online <ichash@proton.me>
2024-11-17 20:53:12 -0700

    add quail system and galina user

---
+++ users/galina.nix
@@ -0,0 +1,39 @@
+{ config, inputs, ... }:
+let
+  secretsElias = builtins.toString inputs.secrets-elias;
+in {
+
+  config = {
+
+    sops = {
+      defaultSopsFile = "${secretsElias}/secrets/galina.yaml";
+      secrets = {
+        password-hash.neededForUsers = true;
+	quail-ssh-key = if builtins.toString config.networking.hostName == "quail" then {
+	  mode = "0600";
+	  owner = "galina";
+	  path = "/home/galina/.ssh/id_ed25519";
+	} else {};
+      };
+    };
+
+    systemd.tmpfiles.rules = [
+      "d /home/galina/.ssh 0700 galina users -"
+    ];
+
+    services.displayManager.autoLogin = {
+      enable = true;
+      user = "galina";
+    };
+
+    users.users."galina" = {
+      isNormalUser = true;
+      hashedPasswordFile = config.sops.secrets.password-hash.path;
+      description = "Galina";
+      extraGroups = [ "networkmanager" "wheel" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIELnLG7uX0hFQ35gKBQG+YwSfXFnsHxcmtSNOxMiFEjl elias@ibis"
+      ];
+    };
+  };
+}
