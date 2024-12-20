46763d6739e8af35e984f68a2be28e9a456efbed
elias-online <ichash@proton.me>
2024-12-20 09:15:11 -0800

    enable tailscale on chick

--- systems/chick.nix
+++ systems/chick.nix
@@ -1,5 +1,7 @@
 { inputs, modulesPath, ... }: {
 
+  sops.secrets.chick-ts-key = {};
+
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     inputs.hardware.nixosModules.common-cpu-intel
@@ -7,6 +9,10 @@
 
     (import ./hardware/single.nix {
       primary = "/dev/disk/by-id/ata-SanDisk_SD8SN8U-512G-1006_172330802078"; #Silver Test Laptop
+    })
+
+    (import ./hardware/vpn.nix {
+      tskey = config.sops.secrets.chick-ts-key.path;
     })
   ];
 
