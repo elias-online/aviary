e541a7fc5bef200bad462b3001829bc4ed06935f
elias-online <ichash@proton.me>
2024-12-21 16:46:37 -0800

    some testing

--- systems/hardware/initrd-test.nix
+++ systems/hardware/initrd-test.nix
@@ -1,4 +1,34 @@
-{ config, pkgs, ... }: {
+{ config, ... }: {
 
-  boot.initrd.network.enable = true;
+  boot = {
+    kernelModules = [ "igb" ];
+
+    initrd = {
+      kernelModules = [ "igb" ];
+
+      systemd = {
+	network = {
+	  enable = true;
+	  networks."enp0s20f0u2u4" = {
+	    enable = true;
+	    name = "enp0s20f0u2u4";
+	    DHCP = "yes";
+	  };
+	};
+      };
+
+      secrets = {
+        "/secrets/boot/ssh/ssh_host_ed25519_key" = "/etc/ssh/ssh_host_ed25519_key";
+      };
+
+      network = {
+        ssh = {
+	  enable = true;
+	  port = 2222;
+	  hostKeys = [ "/secrets/boot/ssh/ssh_host_ed25519_key" ];
+	  authorizedKeys = config.users.users."elias".openssh.authorizedKeys.keys;
+	};
+      };
+    };
+  };
 }
