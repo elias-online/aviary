522ef74264fb9121ce6c58f2092d8b000af5674b
elias-online <ichash@proton.me>
2025-07-27 15:20:48 -0700

    fix path

--- environments/modules/default.nix
+++ environments/modules/default.nix
@@ -264,8 +264,8 @@
         };
       in {
 
-        "overrides.conf"."/run/systemd/generator/dev-mapper-disk\\\\x2dprimary\\\\x2dluks\\\\x2dbtrfs\\\\x2d${mapper}.device.d".f = file;
-        "overrides.conf"."/run/systemd/generator/dev-mapper-disk\\\\x2dsecondary\\\\x2dluks\\\\x2dbtrfs\\\\x2d${mapper}.device.d".f = file;
+        "10-primary"."/run/systemd/generator/dev-mapper-disk\\\\x2dprimary\\\\x2dluks\\\\x2dbtrfs\\\\x2d${mapper}.device.d/overrides.conf".f = file;
+        "20-secondary"."/run/systemd/generator/dev-mapper-disk\\\\x2dsecondary\\\\x2dluks\\\\x2dbtrfs\\\\x2d${mapper}.device.d/overrides.conf".f = file;
       };
  
       services = {
